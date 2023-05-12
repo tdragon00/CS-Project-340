bd9659448b4decbd3a70f907ca96041f367ee6ca
tdragon00 <47230462+tdragon00@users.noreply.github.com>
2023-05-12 16:51:56 -0700

    some more comments
    comment on node.cpp

--- src/node.cpp
+++ src/node.cpp
@@ -1,33 +1,34 @@
 #include "node.h"
 
+//constructor
 template <typename T>
 Node<T>::Node(T data)
 {
     this->data = new T(data);
 }
 
+//destructor
 template <typename T>
 Node<T>::~Node()
 {
     this->next = nullptr;
     this->prev = nullptr;
-
     delete data;
 }
-
+//returns the data
 template <typename T>
 T Node<T>::get_data() const
 {
     return *this->data;
 }
-
+//sets the data 
 template <typename T>
 void Node<T>::set_data(T data)
 {
     delete this->data;
     this->data = new T(data);
 }
-
+//gets the next data
 template <typename T>
 Node<T>* Node<T>::get_next() const
 {
